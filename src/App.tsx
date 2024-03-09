e12392e160ab4ce16f544cf59a94e0f9da22d188
Shulin Zhang <zhangshulin@go.byuh.edu>
2024-03-08 19:17:20 -0700

    Update again

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jsonData from "./CollegeBasketballTeams.json";
 
+class Team extends React.Component {
+  render() {
+    return (
+      <div>
+        {jsonData.teams.map((team, index) => (
+          <div key={index}>
+            <h5>School Name:</h5> <p>{team.school}</p>
+            <h5>Mascot Name:</h5> <p>{team.name}</p>
+            <h5>Location:</h5>{" "}
+            <p>
+              {team.city}, {team.state}
+            </p>
+            <br></br>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+function Heading() {
+  return (
+    <div>
+      <h1>About this Website</h1>
+      <p>
+        This Website lists out information about all the colleges in NCAA
+        Basketball.
+      </p>
+    </div>
+  );
+}
+
+function TeamList() {
+  return (
+    <div>
+      <Team />
+      <Team />
+      <Team />
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+
+      <TeamList />
     </div>
   );
 }
